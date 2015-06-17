4b36bb07cecf4d2d01264944825d87158303c2a8
rmiya <rmiya56@gmail.com>
2015-06-17 13:34:14 +0900

    transplant omp to iterative class

--- OMP.py
+++ OMP.py
@@ -3,8 +3,6 @@ u"""
    OMP (Orthogonal Matching Pursuit) 
 """
 import numpy as np
-from random_matrix import bernoulli, gaussian
-from sparse import sparse
 
 
 u"""
@@ -24,14 +22,23 @@ u"""
 class OMP:
 
     def __init__(self, A, y):
+        # Initialize params
         self.A = A 
         self.y = y
         self.S = set([]) # support set
         self.r = y
+        # Constants 
+        self.EPS         = 10**-4   # acceptable residual
+        self.ITER_MAX    = 10**3    # max of loops 
+        
         
     def next(self):
         "return n-step estimated signal" 
-        return self.iter_omp()
+        x       = self.iter_omp()
+        self.r  = self.y - np.dot(self.A, x)
+        if self.EPS > np.abs( np.linalg.norm(self.r) / np.linalg.norm(self.y) ):
+            print "estimated"
+        return x
     
     def iter_omp(self):    
 
@@ -41,97 +48,49 @@ class OMP:
 
         # B2
         self.S.add(j)
-        #print "self.S", self.S
         
         # B3
-        As  = self.A[:,list(self.S)]  # pick up columns which have the index in S
+        As  = np.compress( [i in self.S for i in np.arange(13)], self.A, axis=1) 
+        #As  = self.A[:,list(self.S)]  # pick up columns which have the index in S
         xs  = np.dot( np.linalg.pinv(As), self.y )  # solve least square
         x   = np.zeros(n, dtype=np.complex)
-        for s, j in enumerate(sorted(self.S)):
-            x[j] = xs[s]
-            
-        self.r = self.y - np.dot(self.A, x)
+        for j, s in enumerate(sorted(self.S)):
+            x[s] = xs[j]
         return x 
      
-
-    
-def omp(A, y):
-    u"""
-        perform OMP
-        [I/O]
-        A: measurement matrix
-        y: measured vector
-        return: recovered vector
-        [flow]
-        A. init values
-        B. iteration
-            B1. find most correlated column
-            B2. append index to the support set
-            B3. calc estimated singal
-            B4. calc residual
-    """
-
-    m, n    = A.shape
-    M       = np.arange(m)
-    N       = np.arange(n)
-    norm_y  = np.linalg.norm(y) 
-
-    def iter_omp(x):
-
-        # OMP1 ~ find a most correlated column
-        p = np.dot( np.conj(A.T), y-np.dot(A, x) ) 
-        j = np.argmax( np.abs(p) )
-        if not j in S:
-            S.append(j) 
-            #print "S:", S
-
-        # OMP2 
-        As  = np.compress( [i in S for i in N], A, axis=1)  # pick up columns which have the index in S
-        xs  = np.dot( np.linalg.pinv(As), y )      # solve least square
-        x   = np.zeros(n, dtype=np.complex)
-        for s, j in enumerate(sorted(S)):
-            x[j] = xs[s]
-        return x 
-     
-    # 初期状態
-    x   = np.zeros(n, dtype=np.complex)
-    S   = [] # 順序付きセット
- 
-    EPS         = 10**-4   # acceptable residual
-    ITER_MAX    = 10**3    # max of loops 
- 
-    for loop in range(ITER_MAX):
-        x = iter_omp(x)
-        r = np.linalg.norm(y - np.dot(A, x)) / norm_y
-        if np.abs(r) < EPS:
-            print loop, " [loops]"
-            return x
-    return "Failed"
-  
 
     
 if __name__ == '__main__':
  
+    import matplotlib
+    matplotlib.use("TkAgg")
     import matplotlib.pyplot as plt 
+    from random_matrix import bernoulli, gaussian
+    from sparse import sparse
+   
     
-
     m = 7
     n = 13
     s = 2
      
-    A =  bernoulli(m, n)
-    A =  gaussian(m, n)
-    x = sparse(n, s)
-    y = np.dot(A,x)
+    A       =  gaussian(m, n)
+    x       = np.zeros(n)
+    x[3]    = -np.sqrt(5)
+    x[10]   = np.pi
+    y       = np.dot(A,x)
     
-    print "x ", x
-    #print "A ", A
      
     omp = OMP(A, y)
-    
     for i in range(5):
         
         z = omp.next()
+        align = "51" + str(i+1)
+        plt.subplot(align)
         plt.scatter(np.arange(n), x) 
-        plt.bar(np.arange(n), z, 0)
-        plt.show()
+        #plt.bar(np.arange(n), z,0)
+        plt.stem(z)
+
+    plt.show()
+        
+        
+        
